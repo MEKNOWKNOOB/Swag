a17522fecdf80ed3687f69d2934ddfd800ece0e4
MEKNOWKNOOB <149892791+MEKNOWKNOOB@users.noreply.github.com>
2025-12-04 20:53:30 -0500

    Additional funcitonality of Action

--- Assets/Scripts/Components/Actions/Action.cs
+++ Assets/Scripts/Components/Actions/Action.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Action : NetworkComponent
 {
+    // How Long the Action Takes
+    [SerializeField] float useTime = 0.0f;
+
+    // Last Time the Action Started (Cannot be used again until Time > useStartTime + useTime)
+    [SerializeField] float useStartTime = 0.0f;
+
     void Awake()
     {
         RefName = "Action";
@@ -10,9 +16,12 @@ public class Action : NetworkComponent
     /// <summary>
     /// Runs Active
     /// </summary>
+    /// <param name="tool"> referencing Tool being used</param>
     /// <param name="user"> user is NOT the Item class, it's the actual User, Tool should work without an Item class as long its part of an Entity</param>
-    public virtual void Active(NetworkEntity user)
+    public virtual void Active(Tool tool, NetworkEntity user)
     {
-        // Make a child class with what you need
+        if (Time.time < useStartTime + useTime) return; // Still Using
+
+        // Make a child class with what you need (animations, hurtboxes, turning left at the crossroads), just call base first
     }
 }
--- Assets/Scripts/Entities/Tool/Tool.cs
+++ Assets/Scripts/Entities/Tool/Tool.cs
@@ -17,6 +17,6 @@ public class Tool : NetworkComponent
     /// <param name="user"> user is NOT the Item class, it's the actual User, Tool should work without an Item class as long its part of an Entity</param>
     public void Use(NetworkEntity user)
     {
-        ((Action)Entity.NetworkComponents["Action"]).Active(user);
+        ((Action)Entity.NetworkComponents["Action"]).Active(this, user);
     }
 }
