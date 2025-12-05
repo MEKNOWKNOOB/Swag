1bffc983801ecead66bf88bc169ca2718108e50f
KhayniyahCulpepper <hnakajima238@gmail.com>
2025-12-05 13:44:20 -0500

    Imported craft button sprite
    Imported craft button sprite and added enemy animation script to the enemy prefab.

--- Assets/Scripts/Components/EnemyAnimation.cs
+++ Assets/Scripts/Components/EnemyAnimation.cs
@@ -8,7 +8,7 @@ public class EnemyAnimation : NetworkComponent
     private Vector2 moveDirection;
     void Awake()
     {
-        RefName = "animation";
+        RefName = "Animator";
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
