1a2904739503c7afcd3159a69c688f270c6fbb28
KhayniyahCulpepper <hnakajima238@gmail.com>
2025-12-05 01:53:51 -0500

    Animated Enemy Walk
    Implemented enemy animations, blend tree, and script.

---
+++ Assets/Scripts/Components/EnemyAnimation.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class EnemyAnimation : NetworkComponent
+{
+    [SerializeField] private Animator enemyAnimator;
+    private Movement enemyMovement;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 moveDirection;
+    void Awake()
+    {
+        enemyAnimator = GetComponent<Animator>();
+        enemyMovement = GetComponent<Movement>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = enemyMovement.Direction;
+
+        // Flip visually only
+        if (moveDirection.x != 0)
+            spriteRenderer.flipX = moveDirection.x > 0;
+
+        if (moveDirection != Vector2.zero)
+        {
+            UpdateMoveDirection();
+        }
+        PlayMoveAnimation();
+    }
+
+    public void PlayMoveAnimation()
+    {
+        enemyAnimator.SetFloat("MoveDirectionX", moveDirection.x);
+        enemyAnimator.SetFloat("MoveDirectionY", moveDirection.y);
+    }
+    
+    public void UpdateMoveDirection()
+    {
+        if (moveDirection.x == 0 && moveDirection.y != 0)
+        {
+            enemyAnimator.SetFloat("MoveDirection", 0);
+        }
+        else if (moveDirection.y == 0 && moveDirection.x != 0)
+        {
+            enemyAnimator.SetFloat("MoveDirection", 1);
+        }
+    }
+}
