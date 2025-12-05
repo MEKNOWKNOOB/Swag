c99b13b832304a978575842bbebf65141094e80c
Section009 <72355643+Section009@users.noreply.github.com>
2025-12-04 22:33:07 -0500

    hid the menu when you click on play
    jdfslkajdslf

--- Assets/Scripts/ButtonFunction.cs
+++ Assets/Scripts/ButtonFunction.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+
+
 public class ButtonFunction : MonoBehaviour
 {
+    [SerializeField] private GameObject menuCanvas;
     public void PlayGame()
     {
+        menuCanvas.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
     }
 
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene(0);
+        SceneManager.UnloadSceneAsync(2);
     }
 }
